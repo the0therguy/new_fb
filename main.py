567a6cde1050a62b7126ac79f69d7ed6ca0b6ca8
the0therguy <nblues399@gmail.com>
2023-06-06 11:52:31 +0600

    initial commit

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from utils import get_env_value, facebook_login, get_city
 from automate import fb_join_request, get_group_posts
 from bson import ObjectId
 
-
-ENV_PATH = Path(__file__).resolve().parent/ ".env"
+ENV_PATH = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
 
 app = Flask(__name__)
@@ -20,7 +19,8 @@ db_username = get_env_value("DB_USERNAME")
 db_password = get_env_value("DB_PASSWORD")
 dbname = get_env_value("DATABASE_NAME")
 collection_name = get_env_value("COLLECTION_NAME")
-client = MongoClient(f"mongodb+srv://{db_username}:{db_password}@grouptracker.qmpwcd4.mongodb.net/{dbname}?retryWrites=true&w=majority")
+client = MongoClient(
+    f"mongodb+srv://{db_username}:{db_password}@grouptracker.qmpwcd4.mongodb.net/{dbname}?retryWrites=true&w=majority")
 
 fb = FacebookScraper()
 fb_email = get_env_value("FB_EMAIL")
@@ -39,7 +39,7 @@ def get_post():
         group_url = data.get('url') if data.get("url") is not None else ""
         keywords = data.get("keywords") if data.get("keywords") is not None else ""
         print('keywords  ', keywords)
-        post_data = get_group_posts(group_url, keywords, fb_email,fb_password)
+        post_data = get_group_posts(group_url, keywords, fb_email, fb_password)
         if bool(post_data):
             if collection.find_one({'post_id': post_data['post_id']}) is None:
                 # Post is not already in the database, insert it
@@ -49,24 +49,13 @@ def get_post():
             return jsonify({'posts': post_data})
         else:
             # Retrieve all posts from the database and return as JSON response
-            all_posts = []
-            for post in collection.find():
-                all_posts.append({
-                    "user_id": post['user_id'],
-                    "username": post['username'],
-                    "post_text": post['post_text'],
-                    "post_id": post['post_id'],
-                    "group_name": post['group_name'],
-                    "post_url": post['post_url'],
-                    "post_time": post['post_time'],
-                    "comments": post['comments'],
-                    "lives_in": post['lives_in']
-                })
-            return jsonify({'posts': all_posts})
+            result = collection.find_one({}, sort=[('_id', -1)])
+            del result['_id']
+            return jsonify({'posts': result})
     except Exception as exp:
-        print("Error:",repr(exp))
+        print("Error:", repr(exp))
         return jsonify({'Message': f"There were some problem at the server...Please try again after sometime."})
-        
+
 
 @app.route('/join_request', methods=['POST'])
 def join_request():
@@ -75,9 +64,11 @@ def join_request():
     json_message = json.dumps(message)  # Serialize the message to JSON
     return Response(json_message, mimetype='application/json')
 
+
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello world'
 
+
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True, port=5000)
